06d44980f2e7981433c438b2147955b05feb14e4
Dario Arrieta <arrieta.dario@hotmail.com>
2020-06-23 19:13:14 -0500

    Prueba con 200 historias

--- js.js
+++ js.js
@@ -56,7 +56,7 @@ function hideShowCard(id){
     }
 }
 //SHOW ABOUT WITH DETAIL CARD
-function showAbout(){
+function showSoySanjuanero(){
     const cardDetail=$('#cardDetail');
     const cardDetailTitle=$('#cardDetailTitle')
     const cardDetailText=$('#cardDetailText')
@@ -65,8 +65,22 @@ function showAbout(){
 
     cardDetailImg.src='https://i.imgur.com/iAsv8NK.jpg'
     cardDetailTitle.innerHTML='SOBRE NOSOTROS'
-    cardDetailText.innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus itaque enim quasi voluptatum nihil. Nostrum voluptatum corrupti reiciendis repellat quibusdam ex amet aliquid, ratione natus praesentium.'
-    cardDetailTextAdd.innerHTML='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus itaque enim quasi voluptatum nihil. Nostrum voluptatum corrupti reiciendis repellat quibusdam ex amet aliquid, ratione natus praesentium.'
+    cardDetailText.innerHTML='San Juan Nepomuceno es un municipio ubicado en la parte norte del departamento de Bolívar. Rico en cultura, gastronomía e historia. Este municipio además pertenece a la región de Los Montes De María, es reconocido por el Santuario de Flora y Fauna Los Colorados.'
+    cardDetailTextAdd.innerHTML='#SoySanjuanero inicia sus labores en el año 2017, cuando un grupo de amigos locales identifican que, dentro de cada ámbito cultural presente en este municipio, siempre existe una historia que contar, por lo tanto se conforma un grupo de trabajo colectivo que se da a la tarea de investigar, documentar y difundir a través de escritos, imágenes y videos las vivencias que resaltan lo que es ser sanjuanero. 			#SoySanjuanero no tiene color político, su trabajo es netamente independiente, fuera de cualquier grupo de intervención pública o privada. Sin embargo, su raíz principal se sostiene gracias al valor que cada uno de ustedes como sociedad nos brinda para seguir avanzando en este hermoso proyecto.'
+    cardDetail.classList.remove('hide')   
+}
+
+function showSanJuanNepo(){
+    const cardDetail=$('#cardDetail');
+    const cardDetailTitle=$('#cardDetailTitle')
+    const cardDetailText=$('#cardDetailText')
+    const cardDetailTextAdd=$('#cardDetailTextAdd')
+    const cardDetailImg=$('#cardDetailImg')
+
+    cardDetailImg.src='https://i.imgur.com/vAGXP1z.png'
+    cardDetailTitle.innerHTML='SAN JUAN NEPOMUCENO BOLÍVAR'
+    cardDetailText.innerHTML='Yo soy donde es la tierra del mico Colorado, donde la inteligencia es peste y es riega como la verdolaga. '
+    cardDetailTextAdd.innerHTML='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit id laborum voluptas commodi, aperiam quidem temporibus natus consequatur libero quia veniam fuga, praesentium autem sequi rem facere reprehenderit, deleniti quisquam.     Perspiciatis, unde dolorum? Ut nihil fugit repellat sint nostrum eveniet perferendis perspiciatis quia quam corporis saepe, commodi harum consequuntur ex tempora omnis nobis magnam aspernatur officia modi voluptatibus illum sapiente.    Voluptatum quibusdam fugit debitis, laboriosam rem cupiditate nesciunt possimus delectus molestiae porro aliquam officiis quisquam quas tempora obcaecati veritatis nam corrupti iure perferendis ab reiciendis, soluta non! Cum, corporis impedit!    Labore sint illum iusto magni asperiores eos tempora. Pariatur impedit dolores et quisquam ad esse molestiae accusantium perferendis officiis reprehenderit optio, cum reiciendis est maxime labore? Incidunt quaerat quo facilis?    Nesciunt beatae dolores dolorem necessitatibus, cupiditate voluptates doloribus corporis rerum earum voluptatibus impedit doloremque veritatis, asperiores dolor exercitationem sequi blanditiis nisi dignissimos molestias quo vel eligendi incidunt consequatur! Voluptatem, velit.'
     cardDetail.classList.remove('hide')   
 }
 
@@ -76,7 +90,8 @@ function createCards(itemJson){
     for(let item of itemJson){
         createOneCard(i,item.title,item.text,item.img,item.link)
         i++;
-    }     
+    }    
+    console.log(i) 
 }
 
 function createOneCard(i,title,text,img,link){
